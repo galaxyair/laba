a7818f0765336492a24e086c5edc9deca81ff3f9
Evgeniy Permiakov <43198225+galaxyair@users.noreply.github.com>
2023-06-01 01:33:40 +0300

    Update docker_running.sh

--- docker_running.sh
+++ docker_running.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker build -t web
+sudo docker build -t web .
 sudo docker images
 sudo docker run -dit -p 81:80 web:latest
 sudo docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
